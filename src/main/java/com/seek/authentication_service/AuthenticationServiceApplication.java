143bc4107fd99cae92647d9aa1172a855d8f9067
Gandhy Leandro Cuasapas <leandritoo0803@gmail.com>
2024-09-26 17:53:48 -0500

    feat: to deploy

--- src/main/java/com/seek/authentication_service/AuthenticationServiceApplication.java
+++ src/main/java/com/seek/authentication_service/AuthenticationServiceApplication.java
@@ -1,13 +1,17 @@
 package com.seek.authentication_service;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class AuthenticationServiceApplication {
 
     public static void main(String[] args) {
+        log.info("Starting application init");
         SpringApplication.run(AuthenticationServiceApplication.class, args);
+        log.info("Starting application end");
     }
 
 }
